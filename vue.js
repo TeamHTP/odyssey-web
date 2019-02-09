8800cd8ca62dbc23bdb105f2c0eedf9ed92ed49c
Tianyi Ma <tn.ma@outlook.com>
2019-02-09 02:29:22 -0600

    API calling method

--- vue.js
+++ vue.js
@@ -1,3 +1,19 @@
+var API_ROOT = 'https://odyssey.teamhtp.com/api/';
+
+function callApi(endpoint, paramMap) {
+    var xhr = new XMLHttpRequest();
+    var queryString = '?';
+    for (var i in paramMap) {
+        queryStr += `${i}=${paramMap[i]}&`
+    }
+    xhr.open('GET', `${API_ROOT}${endpoint}?${queryStr}`, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            callback(JSON.parse(xhr.responseText));
+        }
+    };
+    xhr.send();
+}
 
 var app = new Vue({
     el: '#app',
@@ -6,8 +22,7 @@ var app = new Vue({
     },
     methods: {
         test: function () {
-            console.log(this.initial.radius)
+            console.log(this.initial.radius);
         }
     }
-  })
-
+});
